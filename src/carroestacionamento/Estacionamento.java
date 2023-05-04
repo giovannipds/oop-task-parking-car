4eb9b06c78397bddfba25fd69a81655f718c1ac7
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-05-04 19:39:44 -0300

    getters setters estacionamento

--- src/carroestacionamento/Estacionamento.java
+++ src/carroestacionamento/Estacionamento.java
@@ -13,4 +13,60 @@ public class Estacionamento {
     private String modelo;
     private Tempo horaEntrada;
     private Tempo horaSaida;
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the horaEntrada
+     */
+    public Tempo getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    /**
+     * @param horaEntrada the horaEntrada to set
+     */
+    public void setHoraEntrada(Tempo horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    /**
+     * @return the horaSaida
+     */
+    public Tempo getHoraSaida() {
+        return horaSaida;
+    }
+
+    /**
+     * @param horaSaida the horaSaida to set
+     */
+    public void setHoraSaida(Tempo horaSaida) {
+        this.horaSaida = horaSaida;
+    }
 }
