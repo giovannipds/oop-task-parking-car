2f2ee54064e3eb9790d33e5b7cdf686cf4e6c6ce
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-05-04 19:38:56 -0300

    add estacionamento

---
+++ src/carroestacionamento/Estacionamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carroestacionamento;
+
+/**
+ *
+ * @author giovannipds
+ */
+public class Estacionamento {
+    private String placa;
+    private String modelo;
+    private Tempo horaEntrada;
+    private Tempo horaSaida;
+}
